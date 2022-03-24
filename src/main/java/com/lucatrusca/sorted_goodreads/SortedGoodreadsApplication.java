fffe5ffb8d2e57bc1af5b63e0f9b4bdd0e1e17b8
Luca1152 <t.luca1152@gmail.com>
2022-03-24 17:53:12 +0200

    Initial commit
    Added the initial code, generated using start.spring.io.

---
+++ src/main/java/com/lucatrusca/sorted_goodreads/SortedGoodreadsApplication.java
@@ -0,0 +1,13 @@
+package com.lucatrusca.sorted_goodreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortedGoodreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortedGoodreadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucatrusca/sorted_goodreads/SortedGoodreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatrusca.sorted_goodreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortedGoodreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
